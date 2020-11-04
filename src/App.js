bbbae7855578fe3b7ed4a1bc6394fa2c1bbac512
John Alonso <jvalon9455@gmail.com>
2020-11-03 22:16:23 -0500

    created react app and uploaded to gh pages. Linked home/contact/portfolio and displayed page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
+import Contact from "./containers/Contact/Contact";
+import Home from "./containers/Home/Home";
+import Portfolio from "./containers/Portfolio/Portfolio";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <Router>
+      <div>
+      <Link to ="/">Home</Link>
+      </div>
+      <div>
+      <Link to ="/portfolio">Portfolio</Link>
+      </div>
+      <div>
+      <Link to ="/contact">Contact</Link>
+      </div>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/portfolio" component={Portfolio} />
+        <Route exact path="/contact" component={Contact} />
+      </Switch>
+     
+
+      </Router>
     </div>
   );
 }
---
+++ src/containers/Home/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <div>This is my home page</div>
+        </div>
+    );
+};
+
+export default Home;
