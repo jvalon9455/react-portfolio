83e9bdfe5e2681d4689b8d1f28224d14c66d0d24
John Alonso <jvalon9455@gmail.com>
2020-11-04 21:02:28 -0500

    added navbar and rearramged folders

--- src/App.js
+++ src/App.js
@@ -1,27 +1,28 @@
-import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
-import Contact from "./containers/Contact/Contact";
-import Home from "./containers/Home/Home";
-import Portfolio from "./containers/Portfolio/Portfolio";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Contact from "./pages/Contact/Contact";
+import About from "./pages/About/About";
+import Portfolio from "./pages/Portfolio/Portfolio";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
-       <Router>
-      <div>
-      <Link to ="/">Home</Link>
-      </div>
-      <div>
-      <Link to ="/portfolio">Portfolio</Link>
-      </div>
-      <div>
-      <Link to ="/contact">Contact</Link>
-      </div>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/portfolio" component={Portfolio} />
-        <Route exact path="/contact" component={Contact} />
-      </Switch>
-     
+      <Router>
+        <Navbar />
+        <div>
+          <Link to="/">About</Link>
+        </div>
+        <div>
+          <Link to="/portfolio">Portfolio</Link>
+        </div>
+        <div>
+          <Link to="/contact">Contact</Link>
+        </div>
+        <Switch>
+          <Route exact path="/" component={About} />
+          <Route exact path="/portfolio" component={Portfolio} />
+          <Route exact path="/contact" component={Contact} />
+        </Switch>
 
       </Router>
     </div>
